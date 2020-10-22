4193848fe37d40092ce63524ea0eca109bf5e9a9
Marcio Gonçalves Ribeiro <71527221+MRibeiro22@users.noreply.github.com>
2020-10-21 22:25:14 -0300

    Novo Sync

--- questão_08.js
+++
@@ -1,6 +0,0 @@
-var c = 1
-while (c <= 20 ) {
-    console.log(`Passo ${c}`)
-    c++
-}   
-    console.log('Somando os valores') 
--- questão_11.js
+++
@@ -1,7 +0,0 @@
-var c = 1
-do {
-    console.log(`${c}`)
-    c++
-} while (c <= 20 )
-
-    console.log('Somando os valores' ) 
